44c89da01348481451820ab406880cb01678ff3e
Fantomas42 <fantomas42@gmail.com>
2010-04-12 16:30:56 +0200

    correcting translation and disabling future fields

--- minipoll/models.py
+++ minipoll/models.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 from django.db import models
 from django.contrib.auth.models import User
-from django.utils.translation import ugettext as _
+from django.utils.translation import ugettext_lazy as _
 
 from minipoll.managers import PollPublishedManager
 from minipoll.managers import DRAFT, PUBLISHED, ARCHIVED
@@ -17,7 +17,7 @@ class Poll(models.Model):
     """A simple poll"""
     title = models.CharField(_('title'), max_length=250)
     description = models.TextField(_('description'), blank=True)
-    open_response = models.BooleanField(_('open response'))
+    #open_response = models.BooleanField(_('open response'))
 
     slug = models.SlugField(_('slug'))
     creator = models.ForeignKey(User, verbose_name=_('creator'))
@@ -77,8 +77,8 @@ class Vote(models.Model):
     """A vote for a poll"""
     poll = models.ForeignKey(Poll, verbose_name=_('poll'))
     choice = models.ForeignKey(Choice, verbose_name=_('choice'))
-    comment = models.CharField(_('comment'), max_length=250,
-                               blank=True)
+    #comment = models.CharField(_('comment'), max_length=250,
+    #                           blank=True)
 
     voter = models.ForeignKey(User, verbose_name=_('voter'),
                               blank=True, null=True)
